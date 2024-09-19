8dc3049d8e729e4abd723a39a12b62987433b09e
Charlie <charlie.elizarbe@unmsm.edu.pe>
2024-09-19 18:59:35 -0500

    Refactorización de rutas en el componente AppRoutes
    Se refactorizaron las rutas en el componente AppRoutes para utilizar la nueva API de react-router-dom. Se reemplazó el componente BrowserRouter por createBrowserRouter y se envolvió el componente App con RouterProvider para proporcionar el enrutador. Esto mejora la organización y mantenibilidad del código.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,23 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../views/Home";
 import MovieDetails from "../views/MovieDetails";
 import NotFound from "../views/NotFound";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const AppRoutes: React.FC = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/movie/:id' element={<MovieDetails />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </Router>
-  );
-};
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/movie/:id",
+    element: <MovieDetails />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
 
-export default AppRoutes;
+const App = () => <RouterProvider router={router} />;
+
+export default App;
